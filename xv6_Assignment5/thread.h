71988c80bd886097418ff065acd4d6715a4333c4
99DHL <eudh1206@naver.com>
2021-01-21 14:04:51 +0900

    2018 all

---
+++ xv6_Assignment5/thread.h
@@ -0,0 +1,18 @@
+/* thread.h */
+
+/*************************************************
+ * EEE3535-02 Fall 2018                          *
+ * School of Electrical & Electronic Engineering *
+ * Yonsei University, Seoul, South Korea         *
+ *************************************************/
+
+// Thread struct
+struct thread {
+  int tid;
+};
+
+// Lock struct
+struct mutex {
+  int flag;
+};
+
